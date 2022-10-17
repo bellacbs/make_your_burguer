9f5c5e4f722771be4d7a67f23f356c1b18d9b985
bellacbs <izabellacbrandao@gmail.com>
2022-10-17 14:55:32 -0300

    atualização de pedido

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -15,13 +15,13 @@ export default {
 
 <style scoped>
     .message-container {
-    color: #004085;
-    background-color: #cce5ff;
-    border: 2px solid #b8daff;
-    border-radius: 5px;
-    padding: 10px;
-    max-width: 400px;
-    margin: 30px auto;
-  }
+        color: #004085;
+        background-color: #cce5ff;
+        border: 2px solid #b8daff;
+        border-radius: 5px;
+        padding: 10px;
+        max-width: 400px;
+        margin: 30px auto;
+    }
 
 </style>
